bddb97a0b7d9da1be099b17c33cfe66970c3b72f
daksh23gupta10 <daksh23gupta10@gmail.com>
2026-04-22 16:41:08 +0530

    Create p3.java

---
+++ p3.java
@@ -0,0 +1,33 @@
+
+
+import java.util.Scanner;
+
+class Problem3 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Inputs
+        System.out.print("Enter first number: ");
+        int number1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int number2 = input.nextInt();
+
+        System.out.print("Enter third number: ");
+        int number3 = input.nextInt();
+
+        // Conditions
+        boolean isFirstLargest = (number1 > number2) && (number1 > number3);
+        boolean isSecondLargest = (number2 > number1) && (number2 > number3);
+        boolean isThirdLargest = (number3 > number1) && (number3 > number2);
+
+        // Output
+        System.out.println("Is the first number the largest? " + isFirstLargest);
+        System.out.println("Is the second number the largest? " + isSecondLargest);
+        System.out.println("Is the third number the largest? " + isThirdLargest);
+
+        input.close();
+    }
+}
